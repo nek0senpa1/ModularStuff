e9a39bd65f6c8a494793173b6f7b8564a0187a56
amberm <nekosenpai@gmail.com>
2019-01-13 14:51:01 -0700

    aking a faux fake facebookgram

--- Modulesite/src/App.js
+++ Modulesite/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Widget1 from './components/DisplayComponents/CalculatorDisplay'
+import Widget1 from './components/Top'
 import RegNum, { BigNum } from './components/ButtonComponents/NumberButton';
 import Oper from './components/ButtonComponents/ActionButton';
 
@@ -8,39 +8,10 @@ const App = () => {
   return (
     <div class='container'>
 
-      <h3>Welcome to React Calculator</h3>
       <Widget1 />
 
-      <div class ='row'>
-      <BigNum num ="CLEAR"/>
-      <Oper num = "÷"/>
-      </div>
+     
       
-      <div class ='row'>
-      <RegNum num ="1"/>
-      <RegNum num ="2"/>
-      <RegNum num ="3"/>
-      <Oper num = "x"/>
-      </div>
-      
-      <div class ='row'>
-      <RegNum num ="4"/>
-      <RegNum num ="5"/>
-      <RegNum num ="6"/>
-      <Oper num = "-"/>
-      </div>
-      
-      <div class ='row'>
-      <RegNum num ="7"/>
-      <RegNum num ="8"/>
-      <RegNum num ="9"/>
-      <Oper num = "+"/>
-      </div>
-
-      <div class ='row'>
-      <BigNum num ="0"/>
-      <Oper num = "="/>
-      </div>
 
      
       
--- Modulesite/src/components/Top.js
+++ Modulesite/src/components/Top.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import './Display.css';
+
 
 const display = () => 
 <div class="display">
-<h3> 0 </h3>
+<h3> Welcome to the FakeBookGram Site! </h3>
 </div>
 
 export default display;
